1f032f487ba4303ee8a09f439f7dea5ddd98c6cc
LeHuman <47549331+LeHuman@users.noreply.github.com>
2024-12-22 16:39:31 -0600

    fix: :bug: Also run water.py on kill

--- kill.sh
+++ kill.sh
@@ -2,6 +2,8 @@ FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIRECTORY="$(dirname "$FULL_PATH_TO_SCRIPT")"
 cd $SCRIPT_DIRECTORY
 
+python3 water.py kill &
+
 source env/bin/activate
 kasa --host 192.168.1.44 off
 touch ./fail
